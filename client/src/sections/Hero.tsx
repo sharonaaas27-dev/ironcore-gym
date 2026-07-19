9d92088298f3a84aeebb792e3b0f9236419a3914
Sharon <sharonaaas27@gmail.com>
2026-07-19 17:58:07 +0530

    fix: remove animated scroll indicator (yellow bouncing line) from Hero

--- client/src/sections/Hero.tsx
+++ client/src/sections/Hero.tsx
@@ -188,16 +188,6 @@ export default function Hero() {
           </div>
         </div>
       </div>
-
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2">
-        <motion.div
-          animate={{ y: [0, 8, 0] }}
-          transition={{ duration: 2, repeat: Infinity }}
-          className="flex flex-col items-center gap-2"
-        >
-          <div className="h-8 w-[1px] bg-gold-500/50" />
-        </motion.div>
-      </div>
     </section>
   );
 }
